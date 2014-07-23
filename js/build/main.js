028bebda37bca6cbe7a7be43f96f931ba2c52bbb
Paul van Zyl <paul@nonacreative.com>
2014-07-23 23:50:26 +0200

    basic structure

---
+++ js/build/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+
+	/*
+	Fast click  - https://github.com/ftlabs/fastclick
+	- - - - - - - -
+	fixes 300ms delay on mobile browsers
+	NB - certain elements may need a normal click - add the class needs click
+	*/
+	FastClick.attach(document.body);
+
+
+
+	
+
+
+
+});
