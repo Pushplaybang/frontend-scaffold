dd3fe38347869bf11c0e38967d7bf5cbb864e09a
Paul van Zyl <paul@nonacreative.com>
2015-04-11 19:25:19 +0200

    script error + modernisr call update
    Signed-off-by: Paul van Zyl <paul@nonacreative.com>

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -1,13 +1,5 @@
 jQuery(document).ready(function($) {
 
-	/*
-	Fast click  - https://github.com/ftlabs/fastclick
-	- - - - - - - -
-	fixes 300ms delay on mobile browsers
-	NB - certain elements may need a normal click - add the class needs click
-	*/
-	FastClick.attach(document.body);
-
 
 
 	
